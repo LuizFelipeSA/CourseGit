c28f70de9df67ffdd00ad214238f8678894940b0
Luiz Felipe <luiz-felipe71@hotmail.com>
2019-05-13 13:51:10 -0300

    Acrecentada uma mensagem para Bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -4,6 +4,8 @@ namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.ReadLine();
         }
     }
 }
