a8807c233f40ca24d0094df6b01efb8ccb163476
Luiz Felipe <luiz-felipe71@hotmail.com>
2019-05-14 09:44:46 -0300

    adicionado duas linhas ao código

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -3,10 +3,12 @@
 namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("--------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje");
             Console.WriteLine("Tchau");
+            Console.WriteLine("--------------------");
             Console.ReadLine();
         }
     }
